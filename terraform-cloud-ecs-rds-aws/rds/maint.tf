d9e10aabcc80c335b797cb5b1827d9d986653479
Paula-Encinar <pencinarsanz@gmail.com>
2023-01-05 11:55:06 +0100

    Adding ddbb

--- terraform-cloud-ecs-rds-aws/rds/maint.tf
+++ terraform-cloud-ecs-rds-aws/rds/maint.tf
@@ -1,20 +1,27 @@
+resource "aws_db_subnet_group" "mysql-subnet-group"{
+    name = "mysql-subnet-group"
+    subnet_ids = [aws_subnet.public_subnet_1]
+}
+
 resource "aws_db_instance" "development" {
-  identifier = "mysql"
-  allocated_storage         = 5
-  backup_retention_period   = 2
-  backup_window             = "01:00-01:30"
-  maintenance_window        = "sun:03:00-sun:03:30" 
-  multi_az                  = true
-  engine                    = "mysql"
-  engine_version            = "5.7"
-  instance_class            = "db.t2.micro"
-  name                      = "worker_db" 
-  username                  = "worker"
-  password                  = var.db_password
-  port                      = "3306"
-  publicly_accessible       = true
-  skip_final_snapshot       = true
-  delete_automated_backups  = false
+    identifier = "mysql"
+    allocated_storage         = 5
+    backup_retention_period   = 2
+    backup_window             = "01:00-01:30"
+    maintenance_window        = "sun:03:00-sun:03:30" 
+    multi_az                  = true
+    engine                    = "mysql"
+    engine_version            = "5.7"
+    instance_class            = "db.t2.micro"
+    name                      = "worker_db" 
+    username                  = "worker"
+    password                  = var.db_password
+    port                      = "3306"
+    db_subnet_group_name      = aws_db_subnet_group.mysql-subnet-group
+    vpc_security_group_ids    = [aws_security_group.ecs,aws_security_group.rds] 
+    publicly_accessible       = true
+    skip_final_snapshot       = true
+    delete_automated_backups  = false
 
 
 }
